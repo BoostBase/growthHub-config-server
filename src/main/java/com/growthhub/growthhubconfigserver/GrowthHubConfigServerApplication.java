dd438bf1a1573778d022f61692a22e77b1ea159f
csh <seonghun7304@naver.com>
2024-08-04 13:48:39 +0900

    first commit

---
+++ src/main/java/com/growthhub/growthhubconfigserver/GrowthHubConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.growthhub.growthhubconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class GrowthHubConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowthHubConfigServerApplication.class, args);
+    }
+
+}
